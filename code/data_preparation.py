98a33f1ea7b455e7a03604b21cd6ea0877bd634c
martin-borkovec <martin.borkovec@skyforge.at>
2022-03-18 15:24:11 +0100

    deletion of Rstudio project, import of csv file and simple analysis

---
+++ code/data_preparation.py
@@ -0,0 +1,5 @@
+
+#$$
+import pandas as pd
+
+dat = pd.read_csv("data\bquxjob_ee0348b_17f9c8a46db.csv")
---
+++ code/price_analysis.py
@@ -0,0 +1,10 @@
+#%%
+(
+    dat
+    .query('ang_VONLAND == "AT"') # query equals filter in Pandas
+    .filter(["ang_st_transportmittelcode", "ang_stp_gesamtpreis"]) # select the columns Type and Price
+    .groupby("ang_st_transportmittelcode")
+    .agg("mean")
+    .reset_index() 
+    .set_axis(["Type", "averagePrice"], axis = 1, inplace = False)
+)
